df66e73bea672f68dc5e534d264620cc5fd52ef1
Chirica Robert <49200643+robeert77@users.noreply.github.com>
2023-11-28 23:20:33 +0200

    Command handler for set_brightness

--- bot/bot.py
+++ bot/bot.py
@@ -28,6 +28,7 @@ class Bot(object):
 
     def __create_command_handlers(self):
         command_handlers = BotCommandHandlers(bot_instance=self.__bot)
+        message_handler = BotMessageHandlers(bot_instance=self.__bot)
 
         # START handler
         start_handler = CommandHandler('start', command_handlers.start)
@@ -41,7 +42,7 @@ class Bot(object):
         add_allowed_user_handler = ConversationHandler(
             entry_points=[CommandHandler('add_allowed_user', command_handlers.add_allowed_user)],
             states={
-                0: [MessageHandler(filters.FORWARDED | filters.CONTACT, command_handlers.get_user_id)]
+                0: [MessageHandler(filters.FORWARDED | filters.CONTACT, message_handler.get_user_id)]
             },
             fallbacks=[],
         )
@@ -51,6 +52,16 @@ class Bot(object):
         remove_users = CommandHandler('remove_allowed_user', command_handlers.remove_allowed_user)
         self.__application.add_handler(remove_users)
         self.__application.add_handler(CallbackQueryHandler(command_handlers.handle_remove_account_button))
+
+        # SET LEDS BRIGHTNESS handler
+        add_allowed_user_handler = ConversationHandler(
+            entry_points=[CommandHandler('set_brightness', command_handlers.set_brightness)],
+            states={
+                0: [MessageHandler(filters.TEXT, message_handler.set_brightness)]
+            },
+            fallbacks=[],
+        )
+        self.__add_handler(add_allowed_user_handler)
 
     def __create_message_handlers(self):
         message_handler = BotMessageHandlers(bot_instance=self.__bot)
--- bot/handlers/message_handler.py
+++ bot/handlers/message_handler.py
@@ -1,6 +1,7 @@
 from .base_handlers import BaseHandlers
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler, CallbackContext
+
 
 class BotMessageHandlers(BaseHandlers):
     def __init__(self, bot_instance=None):
@@ -10,16 +11,60 @@ class BotMessageHandlers(BaseHandlers):
         if not await self.can_respond(update):
             return
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=update.message.text
-        )
+        await self.send_response_message(update.message.text, update, context)
 
     async def unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if not await self.can_respond(update):
             return
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text='Sorry, I didn\'t understand that command.'
-        )
+        message = 'Sorry, I didn\'t understand that command.'
+        await self.send_response_message(message, update, context)
+
+    async def get_user_id(self, update: Update, context: CallbackContext):
+        if not await self.can_respond(update):
+            return
+
+        forwarded_message = update.message.forward_from
+        contact = update.message.contact
+
+        user = None
+        if forwarded_message is not None and hasattr(forwarded_message, 'id'):
+            user = await self._user_service.get_user_object(forwarded_message['id'])
+        elif contact is not None and hasattr(contact, 'user_id'):
+            user = await self._user_service.get_user_object(contact['user_id'])
+
+        if user is None:
+            message = 'Invalid user. Please try again!'
+            await self.send_response_message(message, update, context)
+
+            return
+
+        if self._settings.add_allowed_user(user):
+            message = 'User has been added with success!'
+        else:
+            message = 'User already has been allowed to chat!'
+
+        await self.send_response_message(message, update, context)
+
+        return ConversationHandler.END
+
+    async def set_brightness(self, update: Update, context: CallbackContext):
+        if not await self.can_respond(update):
+            return
+
+        brightness_value = update.message.text
+        if brightness_value.isdigit() and 0 <= int(brightness_value) <= 255:
+            brightness_value = int(brightness_value)
+        else:
+            message = 'Please enter a number between 0 and 255!'
+            await self.send_response_message(message, update, context)
+            return 0
+
+        # TODO
+        # Send brightness_value to the led module
+
+        message = f'Brightness was set to {brightness_value}'
+        await self.send_response_message(message, update, context)
+
+        return ConversationHandler.END
+
